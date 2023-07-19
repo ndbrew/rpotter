0e3502c2e9d7cf3331d1acade7abca90bf7f8150
ndbrew <ndbsignups+gitea@gmail.com>
2023-07-18 23:15:10 -0700

    Add better image filtering

--- rpotter.py
+++ rpotter.py
@@ -39,15 +39,15 @@ MAX_RECONNECT_DELAY = 60
 lk_params = dict( winSize  = (20,20),
                 maxLevel = 2,
                 criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-# blur_params = (4,4)
+blur_params = (5,5)
 dilation_params = (5, 5)
-movment_threshold = 10
+movment_threshold = 2
 
 # start capturing
 CAM_W_RES=640
 CAM_H_RES=480
-
 POINT_COLOR=(0,0,255)
+WAND_TRESHOLD = 100
 
 ig = [[0] for x in range(20)]
 
@@ -119,6 +119,8 @@ def Spell(spell):
 
 def IsGesture(a,b,c,d,i):
     #look for basic movements - TODO: trained gestures
+    os.system('clear')
+    print(f'Point {i}')
     if ((a<(c-5))&(abs(b-d)<2)):
         ig[i].append("left")
     elif ((c<(a-5))&(abs(b-d)<2)):
@@ -140,17 +142,9 @@ def IsGesture(a,b,c,d,i):
     #    Spell("Colovaria")
     # elif "leftup" in astr:
     #     Spell("Aguamenti")
+    print(astr)
     if len(ig[i]) > 10:
-        ig[i]=[]    
-    #print(astr)
-
-def print_wand_movement():
-    global ig
-    os.system('clear')
-    for i,igg in enumerate(ig):
-        astr = ''.join(map(str,igg))
-        print(f"point {str(i)}: {astr}")
-    threading.Timer(.2, print_wand_movement).start()
+        ig[i]=[]
 
 def capture_frame():
     _, frame = cam.read()
@@ -162,38 +156,47 @@ def gen_frame_gray(frame):
 
     #frame_gray = decrease_brightness(frame_gray, value=30)
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.equalizeHist(frame_gray)
-    frame_gray = cv2.GaussianBlur(frame_gray,(9,9),1.5)
+    #cv2.equalizeHist(frame_gray)
+    frame_gray = cv2.GaussianBlur(frame_gray,blur_params,1.5)
     dilate_kernel = np.ones(dilation_params, np.uint8)
     frame_gray = cv2.dilate(frame_gray, dilate_kernel, iterations=1)
-    frame_clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-    frame_gray = frame_clahe.apply(frame_gray)
+    #frame_clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+    #frame_gray = frame_clahe.apply(frame_gray)
     dmask = np.zeros_like(frame_gray)
     cv2.rectangle(dmask,(300,220),(680,320),255,cv2.FILLED)
     frame_gray = cv2.bitwise_and(frame_gray,dmask)
     return frame_gray
 
-def wand_overlay(frame,points):
+def wand_overlay(f,points):
+    f2 = f.copy()
     for i,p in enumerate(points):
         a,b = p.ravel()
-        cv2.circle(frame,(a,b),5,POINT_COLOR,-1)
-        cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, POINT_COLOR)
-    return frame
+        cv2.circle(f2,(a,b),5,POINT_COLOR,-1)
+        cv2.putText(f2, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, POINT_COLOR)
+    return f2
+
 
 def FindWand():
     global p0, cam, mask, ig
     try:
+        start = time.time()
         frame = capture_frame()
         frame_gray = gen_frame_gray(frame)
-        p0 = cv2.HoughCircles(frame_gray,cv2.HOUGH_GRADIENT,3,50,param1=240,param2=8,minRadius=3,maxRadius=15)
-        if p0 is not None:
-            print("Points Found")
+        p0 = cv2.HoughCircles(frame_gray,cv2.HOUGH_GRADIENT,3,50,param1=340,param2=4,minRadius=3,maxRadius=15)
+        if len(p0) > 0:
             p0.shape = (p0.shape[1], 1, p0.shape[2])
             p0 = p0[:,:,0:2]
+            adj_index=0
+            for i,p in enumerate(p0):
+                a,b = p.ravel()
+                if not frame_gray[int(b)][int(a)] >= WAND_TRESHOLD:
+                    p0 = np.delete(p0,i-adj_index,0)
+                    adj_index=adj_index+1
             ig = [[0] for x in range(20)]
-            mask = np.zeros_like(frame)
-        print("finding...")
-        threading.Timer(5, FindWand).start()
+        mask = np.zeros_like(frame_gray)
+        end = time.time()
+        #print(f"FindWand Completed in {end - start} seconds.")
+        threading.Timer(3, FindWand).start()
     except:
         e = sys.exc_info()[1]
         print("Error: %s" % e) 
@@ -202,53 +205,56 @@ def FindWand():
 def TrackWand():
     global cam, p0, mask
     FindWand()
-    print_wand_movement()
     old_gray = None
     while True:
         try:
-            if p0 is not None:
-                frame = capture_frame()
-                frame_gray = gen_frame_gray(frame)
-                if old_gray is not None:
-                    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-                    # p2 = cv2.HoughCircles(old_gray,cv2.HOUGH_GRADIENT,3,50,param1=240,param2=30,minRadius=4,maxRadius=10)
-                    # p2 = p2[:,:,0:2]
-                    # p2.shape = (p2.shape[1], 1, p2.shape[2])
+            start = time.time()
+            frame = capture_frame()
+            frame_gray = gen_frame_gray(frame)
+            if old_gray is not None and len(p0) > 0:
+                p1, st, _ = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+                # p2 = cv2.HoughCircles(old_gray,cv2.HOUGH_GRADIENT,3,50,param1=240,param2=30,minRadius=4,maxRadius=10)
+                # p2 = p2[:,:,0:2]
+                # p2.shape = (p2.shape[1], 1, p2.shape[2])
 
-                    # for i,circles in enumerate(p2):
-                    #     a,b = circles.ravel()
-                    #     cv2.circle(frame,(a,b),5,color,-1)
-                    #     cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255))
-                    # cv2.imshow("Raspberry Potter", frame)
+                # for i,circles in enumerate(p2):
+                #     a,b = circles.ravel()
+                #     cv2.circle(frame,(a,b),5,color,-1)
+                #     cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255))
+                # cv2.imshow("Raspberry Potter", frame)
 
-                    
-                    # Select good points
-                    good_new = p1[st==1]
-                    good_old = p0[st==1]
+                
+                # Select good points
+                good_new = p1[st==1]
+                good_old = p0[st==1]
 
-                    # draw the tracks
-                    for i,(new,old) in enumerate(zip(good_new,good_old)):
-                        a,b = new.ravel()
-                        c,d = old.ravel()
-                        # only try to detect gesture on highly-rated points (below 10)
+                
+                # draw the tracks
+                for i,(new,old) in enumerate(zip(good_new,good_old)):
+                    a,b = new.ravel()
+                    c,d = old.ravel()
+                    # only try to detect gesture on highly-rated points (below 10)
+                    dist = math.hypot(a - c, b - d)
+                    if (dist>movment_threshold):
+                        cv2.line(mask, (a,b),(c,d),(0,255,0), 2)
                         if (i<10):
                             IsGesture(a,b,c,d,i)
-                        dist = math.hypot(a - c, b - d)
-                        if (dist<movment_threshold):
-                            cv2.line(mask, (a,b),(c,d),(0,255,0), 2)
-                        cv2.circle(frame,(a,b),5,POINT_COLOR,-1)
-                        cv2.putText(frame, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, POINT_COLOR) 
-                    img = cv2.add(frame,mask)
-                    cv2.putText(img, "Press ESC to close.", (5, 25),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
-                    cv2.imshow("img",img)
-                    p0 = good_new.reshape(-1,1,2)
-                cv2.imshow("Raspberry Potter", frame)
-                cv2.imshow("Frame Gray", frame_gray)
-
-                # Now update the previous frame and previous points
-                old_gray = frame_gray.copy()
+                    cv2.circle(frame_gray,(a,b),5,POINT_COLOR,-1)
+                    cv2.putText(frame_gray, str(i), (a,b), cv2.FONT_HERSHEY_SIMPLEX, 1.0, POINT_COLOR) 
+                p0 = good_new.reshape(-1,1,2)
             
+            img = cv2.add(frame_gray,mask)
+                #cv2.putText(img, "Press ESC to close.", (5, 25),
+                #    cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+                #cv2.imshow("img",img)
+                
+            #cv2.imshow("Raspberry Potter", frame)
+            cv2.imshow("Raspberry Potter", img)
+
+            # Now update the previous frame and previous points
+            old_gray = frame_gray.copy()
+            end = time.time()
+            #print(f"TrackWand Completed in {end - start} seconds.")
         except IndexError:
             print("Index error - Tracking")  
         except:
@@ -261,12 +267,13 @@ def TrackWand():
             cam.release()
             break
 
-# Setup Global Camera
+
+print("Initializing Camera")
 cam = cv2.VideoCapture(-1)
 cam.set(3, CAM_W_RES)
 cam.set(4, CAM_H_RES)
 
-try:
+if __name__ == "__main__":
     load_dotenv()
     global topic
     topic = os.getenv('TOPIC')
@@ -277,12 +284,7 @@ try:
     client = connect_mqtt(broker, port)
     client.on_disconnect = on_disconnect
 
-    print("Initializing Camera")
-
-    
     print("Initializing point tracking")
     TrackWand()
-finally:
-    cam.release()
  
 
